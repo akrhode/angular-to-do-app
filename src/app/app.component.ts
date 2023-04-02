b56dcfbf4fd81a6d20d846b19d12327720a9b608
akrhode <a.rhode@web.de>
2023-04-02 12:43:15 +0200

    Add colour function and input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  todos = [
+    { todo: 'Buy groceries', done: false },
+    { todo: 'Water plants', done: false },
+    { todo: 'Clean the flat', done: true },
+  ];
+
+  toggleTodo(index: number) {
+    this.todos[index].done = !this.todos[index].done;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
   title = 'angular-to-do-app';
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  @Input() todo = { todo: '', done: false };
+  @Input() index = 0;
 
+  @Output() todoIndex = new EventEmitter<number>();
+  @Output() deleteIndex = new EventEmitter<number>();
+
+  toggleTodo() {
+    this.todoIndex.emit(this.index);
+  }
+  deleteTodo() {
+    this.deleteIndex.emit(this.index);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
